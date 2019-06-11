cb3ea4d5a315bd83325f92d6162847553480bf06
= <nguyenvan21697@gmail.com>
2019-06-11 22:56:53 +0700

    update css
    Signed-off-by: = <nguyenvan21697@gmail.com>

--- Model/Dao/ProductDao.cs
+++ Model/Dao/ProductDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using Model.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,11 +23,26 @@ namespace Model.Dao
         {
             return db.Products.OrderByDescending(s=>s.CreatedDate).Take(top).ToList();
         }
-        public List<Product> ListByCategory(long categoryID, ref int totalRecord, int pageIndex=1, int pageSize=2)
+        public List<ProductViewModel> ListByCategory(long categoryID, ref int totalRecord, int pageIndex=1, int pageSize=2)
         {
             totalRecord = db.Products.Where(s => s.CategoryId == categoryID).Count();
-            var model= db.Products.Where(s => s.CategoryId == categoryID).OrderByDescending(x=>x.CreatedDate).Skip((pageIndex - 1)* pageSize).Take(pageSize).ToList();
-            return model;
+            var model = from a in db.Products
+                        join b in db.ProductCategories
+                           on a.CategoryId equals b.ID
+                        select new ProductViewModel()
+                        {
+                            CateMetaTitle = b.MetaTitle,
+                            CateName = b.Name,
+                            CreatedDate = a.CreatedDate,
+                            ID = a.ID,
+                            Name = a.Name,
+                            Image= a.Image,
+                            MetaTitle = a.MetaTitle,
+                            Price = a.Price,
+
+                        };
+             model.OrderByDescending(x=>x.CreatedDate).Skip((pageIndex - 1)* pageSize).Take(pageSize).ToList();
+            return model.ToList();
         }
         
         public List<Product> ListFeatureProducts(int top)
